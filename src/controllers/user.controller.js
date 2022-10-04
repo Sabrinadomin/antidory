38927143a5c7045fd2a9691fb488f6dd58d5c29b
Sabrina Conceicao <sabrinadomin14@gmail.com>
2022-10-04 20:36:40 -0300

    Added functions to deal with MongoDB users.

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,70 @@
+const { User } = require('../db/schemas/user')
+const { formatList } = require('../utils/string.utils')
+
+async function addNewUser(ctx) {
+  try {
+    let success = false
+    const newUser = new User({
+      name: `${ctx.update.message.chat.first_name} ${ctx.update.message.chat.last_name}`,
+      telegramId: ctx.update.message.chat.id,
+      list:""
+    })
+
+    await newUser.save()
+      .then(success = true)
+      .catch(err => console.log(err))
+    
+    return success
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+async function userSearch(ctx) {
+  try {
+    const userSearch = await User.find({ telegramId: ctx.update.message.chat.id })
+    if(userSearch.length != 0) return true 
+    return false
+
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+async function updateList(ctx, item) {
+  try {
+    const reference = { telegramId: ctx.update.message.chat.id }
+    await User.findOneAndUpdate(reference, { $push: { list: item } })
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+async function userHandle(ctx) {
+  try {
+    const existentUser = await userSearch(ctx)
+    if(existentUser === true) return
+    const createNewUserResult = await addNewUser(ctx)
+    if(createNewUserResult) console.info('Usuário criado')
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+async function getUserList(ctx) {
+  try {
+    const userSearch = await User.find({ telegramId: ctx.update.message.chat.id })
+    if(!userSearch) return 
+    const list = userSearch[0]['_doc'].list
+    const formattedList = formatList(list)
+    return formattedList
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+module.exports = {
+  userHandle,
+  updateList,
+  getUserList
+}
